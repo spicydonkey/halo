9d82de35674a12aeeb58cc3453d067abebc62492
David Shin <dk.shin1992@gmail.com>
2015-11-07 17:40:52 +1100

    create halo simulation script

---
+++ collidinggas.m
@@ -0,0 +1,13 @@
+%% Gas collision simulation
+% A classical simulation of the evolution of the momentum distribution of
+% gas by two-particle collisions
+% DK Shin
+% 07.11.15
+
+close all; clear all;
+
+% momentum distribution parameters
+mean = [0;0;0];
+dev = [1;2;3];
+
+% set up simulation
