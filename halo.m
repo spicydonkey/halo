aab6d9c3e5107d35ea8682de774ef65973e5eab7
David Shin <dk.shin1992@gmail.com>
2015-11-10 23:52:35 +1100

    Implement halo simulation function in the main code

--- halo.m
+++ halo.m
@@ -4,29 +4,40 @@
 % 10.11.15
 
 
+close all; clear all;
+
+
 %% Parameters
-% Statistics
-N_sim;      % number of simulations
+N_sim=5;      % number of simulations
 
 % Halo simulation
-% parameters for halo momentum distribution
+% bec momentum distribution (GAUSSIAN)
+P_dist{1}{1} = [1;0;0];         % BEC1 mean momentum
+P_dist{1}{2} = [0.1;0.1;0.1];   % BEC1 momentum std
+P_dist{2}{1} = -P_dist{1}{1};   % BEC2 mean momentum (experimentally fix global origin as centre of motion)
+P_dist{2}{2} = [0.1;0.1;0.1];   % BEC2 momentum std
+
 % number of collision pairs (can improve by uncertainties and detector qe,
 % etc)
+N_pair=10000;
 
 % Data analysis
-ZONE_F;     % fraction of halo to perform num diff analysis
-% number of "polar" and "azimutal" zones (EVEN numbers to update later)
-N_POLAR;
-N_AZIM;
+zone_frac=1e-2;      % fraction of halo to perform num diff analysis
+% number of "polar" and "azimutal" zones (must be EVEN to update later)
+Nz_polar=3;
+Nz_azim=4;
 
 
 %% Simulation
+N_diff = zeros(Nz_polar*Nz_azim,N_sim);
 for i_sim = 1:N_sim
+    close all;
     % run halo simulation
+    P_halo = halo_sim(P_dist,N_pair);
     
     % find number differences vs. theta,phi in this simulation
     
-    % collate result
+    % collate result in N_diff
 end
 
 
