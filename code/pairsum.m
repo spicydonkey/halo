d5727e113749fa0a40cdb43c02394178e27709d4
David Shin <dk.shin1992@gmail.com>
2015-11-16 22:34:20 +1100

    Improve memory and speed in g2 calculation

--- code/pairsum.m
+++ code/pairsum.m
@@ -2,11 +2,23 @@ function deltaP = pairsum(P_vect, p_delta)
 
 N_p = size(P_vect,2);   % number of particles in momentum space
 
-deltaP = [];
+N = 0;
+P_corr = cell(N_p,1);
 for i=1:N_p
     P_temp = P_vect(:,i+1:end) + P_vect(:,i)*ones(1,N_p-i);     % pair-wise sum
     P_temp = ballfilter(P_temp, p_delta);   % momentum pair filtering 
-    deltaP = [deltaP P_temp];   % keep correlations in specified volume
+    P_corr{i} = P_temp;   % keep correlations in specified volume
+    N = N + size(P_temp,2);
+    if mod(i,round(N_p/100))==0
+        disp([num2str(round(i/round(N_p/100))) '%']);
+    end
+end
+
+deltaP = zeros(3,N);
+index = 1;
+for i=1:N_p
+    deltaP(:,index:(index+size(P_corr{i},2)-1)) = P_corr{i};
+    index = index+size(P_corr{i},2);
 end
 
 return
