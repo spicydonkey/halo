c62da65263d3eacb12844cb6250366f7e55967da
David Shin <dk.shin1992@gmail.com>
2015-11-19 14:55:25 +1100

    Update run script

--- code/run_halo.m
+++ code/run_halo.m
@@ -4,8 +4,14 @@
 
 clear all; close all;
 
-run('sim_config.m');
-run('halo.m');
-run('g2_BB.m');
-run('g2_histo.m');
-run('g2_gauss_fit.m');
+% run('sim_config.m');
+% run('halo.m');
+% run('g2_BB.m');
+% run('g2_histo.m');
+% run('g2_gauss_fit.m');
+
+sim_config;
+halo;
+g2_BB;
+g2_histo;
+g2_gauss_fit;
