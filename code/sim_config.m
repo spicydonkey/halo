aca7b431af08fb054b4452532374284fb86bd432
DKS <david.shin@anu.edu.au>
2015-11-19 11:42:46 +1100

    Fix wavelength

--- code/sim_config.m
+++ code/sim_config.m
@@ -7,7 +7,7 @@
 hbar = 1.055e-34;
 m_He = 6.65e-27;
 a_He = 7.5e-9;      % s-wave scattering length of He*
-k_laser = 2*pi/(1550e-9);   % Raman/Bragg wave number
+k_laser = 2*pi/(1083e-9);   % Raman/Bragg wave number
 
 
 %% Simulation
@@ -22,7 +22,7 @@ N_halo=30;      % number of atoms in detected halo
 N_0 = 1e4;      % number of atoms in condensate
 
 % mean/recoil momentum
-P_dist{1}{1} = hbar*k_laser*[1;0;0];    % BEC1 mean momentum
+P_dist{1}{1} = hbar*k_laser*[0;0;1];    % BEC1 mean momentum
 P_dist{2}{1} = -P_dist{1}{1};   % BEC2 mean momentum (experimentally fix global origin as centre of motion)
 P_norm = norm(P_dist{1}{1});    % BEC com momentum (recoil velocity from Bragg/Raman) to normalise all other momenta
 
