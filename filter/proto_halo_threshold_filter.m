ce0cfea4f22b3ad32a8d7ecb07a0ebb0355ec373
David <dk.shin1992@gmail.com>
2018-01-11 17:21:18 +1100

    halo filter script simplified and tidied

--- filter/proto_halo_threshold_filter.m
+++ filter/proto_halo_threshold_filter.m
@@ -35,6 +35,7 @@ El_noise=cellfun(@(b)El(b),b_noisy_zone,'UniformOutput',false);
 % combined noisy region
 b_noisy_zone_comb=or(b_noisy_zone{:});
 
+
 %% filter density map
 n_filt=n;
 for ii=1:2
@@ -55,40 +56,46 @@ for ii=1:2
 end
 
 
-%% filter halo
-% find counts near noisy region
-S=cellfun(@(x)zxy2sphpol(x),K,'UniformOutput',false);
-b_bad_count=cellfun(@(s)false(size(s,1),1),S,'UniformOutput',false);    % initialise bad counts
-for mm=1:2
-    tS=S(:,mm);
-    bb_bad=cellfun(@(s)false(size(s,1),1),tS,'UniformOutput',false);     % initialise bad counts
-    for ii=1:length(Az_noise{mm})
-        taz=Az_noise{mm}(ii);
-        tel=El_noise{mm}(ii);
-        
-        % get indices to counts in this noisy zone
-        tb_bad=cellfun(@(s) inZone(taz,tel,s(:,1),s(:,2),dpsi_lim),tS,'UniformOutput',false);
-        
-        % update accumulator
-        bb_bad=cellfun(@(b1,b2)b1|b2,bb_bad,tb_bad,'UniformOutput',false);
-    end
-    b_bad_count(:,mm)=bb_bad;      % store
-end
+%% filter bell corr
+E_orig=E;
+E_filt=E;
+E_filt(b_noisy_zone_comb)=NaN;
 
-fprintf('number of counts in noisy region: %d \n',sum(cellfun(@(b) sum(b),b_bad_count)));
 
-% filter the bad counts
-% NOTE: atom ordering is unchanged between cart <--> spherical transform
-K_bad=cellfun(@(k,b) k(b,:),K,b_bad_count,'UniformOutput',false);     % bad k-vecs
-K_filt=cellfun(@(k,b) k(~b,:),K,b_bad_count,'UniformOutput',false);     % filtered k-vecs
-
-% display
-figure();
-plot_zxy(K_bad,[],100);
-hold on;
-plot_zxy(K_filt,[],10);
-axis equal;
-box on;
-xlabel('$K_x$','interpreter','latex');
-ylabel('$K_y$','interpreter','latex');
-zlabel('$K_z$','interpreter','latex');
+% %% filter halo
+% % find counts near noisy region
+% S=cellfun(@(x)zxy2sphpol(x),K,'UniformOutput',false);
+% b_bad_count=cellfun(@(s)false(size(s,1),1),S,'UniformOutput',false);    % initialise bad counts
+% for mm=1:2
+%     tS=S(:,mm);
+%     bb_bad=cellfun(@(s)false(size(s,1),1),tS,'UniformOutput',false);     % initialise bad counts
+%     for ii=1:length(Az_noise{mm})
+%         taz=Az_noise{mm}(ii);
+%         tel=El_noise{mm}(ii);
+%         
+%         % get indices to counts in this noisy zone
+%         tb_bad=cellfun(@(s) inZone(taz,tel,s(:,1),s(:,2),dpsi_lim),tS,'UniformOutput',false);
+%         
+%         % update accumulator
+%         bb_bad=cellfun(@(b1,b2)b1|b2,bb_bad,tb_bad,'UniformOutput',false);
+%     end
+%     b_bad_count(:,mm)=bb_bad;      % store
+% end
+% 
+% fprintf('number of counts in noisy region: %d \n',sum(cellfun(@(b) sum(b),b_bad_count)));
+% 
+% % filter the bad counts
+% % NOTE: atom ordering is unchanged between cart <--> spherical transform
+% K_bad=cellfun(@(k,b) k(b,:),K,b_bad_count,'UniformOutput',false);     % bad k-vecs
+% K_filt=cellfun(@(k,b) k(~b,:),K,b_bad_count,'UniformOutput',false);     % filtered k-vecs
+% 
+% % display
+% figure();
+% plot_zxy(K_bad,[],100);
+% hold on;
+% plot_zxy(K_filt,[],10);
+% axis equal;
+% box on;
+% xlabel('$K_x$','interpreter','latex');
+% ylabel('$K_y$','interpreter','latex');
+% zlabel('$K_z$','interpreter','latex');
